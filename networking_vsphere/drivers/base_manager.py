a1e6171a6ecaaebadb8e2953211bb25635a7b5e7
Rashmi <prashmi@hp.com>
2015-01-09 15:15:16 +0530

    Driver and Manager framework
    This patchset defines interface required for
    Driver and Manager.
    
    Change-Id: I78af0e95a871834872f63b35342a7f95421e9d70

---
+++ networking_vsphere/drivers/base_manager.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class DriverManager(object):
+
+    '''Base class for all driver managers.'''
+
+    def __init__(self):
+        self.driver = None
+
+    def get_driver(self):
+        return self.driver
+
+    def initialize_driver(self):
+        '''Initialize the driver managed by this manager.'''
+        raise NotImplementedError()
+
+    def handle_conf_update(self):
+        '''Handle configuration changes applicable to the managed driver.'''
+        raise NotImplementedError()
+
+    def start(self):
+        '''Start driver action when node is up.'''
+        return
+
+    def pause(self):
+        '''Handle pause action.'''
+        return
+
+    def stop(self):
+        '''Handle stop action.'''
+        return
--- networking_vsphere/tests/unit/drivers/fake_driver.py
+++ networking_vsphere/tests/unit/drivers/fake_driver.py
@@ -12,9 +12,9 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-#
 
 from networking_vsphere.drivers import driver as network_driver
+from networking_vsphere.drivers import driver as network_driver_callback
 
 
 class FakeInvalidDriver(object):
@@ -23,3 +23,82 @@ class FakeInvalidDriver(object):
 
 class FakeNetworkDriver(network_driver.NetworkDriver):
     pass
+
+
+class MockNetworkDriver(network_driver.NetworkDriver):
+
+    def __init__(self):
+        super(MockNetworkDriver, self).__init__()
+        self.methods = {}
+
+    def create_network(self, network, virtual_switch):
+        kwargs = {}
+        kwargs["network"] = network
+        kwargs["virtual_switch"] = virtual_switch
+        self.methods["create_network"] = kwargs
+
+    def create_port(self, network, port, virtual_nic):
+        kwargs = {}
+        kwargs["network"] = network
+        kwargs["port"] = port
+        kwargs["virtual_nic"] = virtual_nic
+        self.methods["create_port"] = kwargs
+
+    def update_port(self, network=None, port=None, virtual_nic=None):
+        kwargs = {}
+        kwargs["network"] = network
+        kwargs["port"] = port
+        kwargs["virtual_nic"] = virtual_nic
+        self.methods["update_port"] = kwargs
+
+    def delete_network(self, network, virtual_switch=None):
+        kwargs = {}
+        kwargs["network"] = network
+        kwargs["virtual_switch"] = virtual_switch
+        self.methods["delete_network"] = kwargs
+
+    def post_delete_vm(self, vm):
+        kwargs = {}
+        kwargs["vm"] = vm
+        self.methods["post_delete_vm"] = kwargs
+
+    def get_pg_vlanid(self, dvs_name, pg_name):
+        kwargs = {}
+        kwargs["dvs_name"] = dvs_name
+        kwargs["pg_name"] = pg_name
+        self.methods["get_pg_vlanid"] = kwargs
+
+    def get_vm_ref_uuid(self, vm_uuid):
+        kwargs = {}
+        kwargs["vm_uuid"] = vm_uuid
+        self.methods["get_vm_ref_uuid"] = kwargs
+
+    def wait_for_portgroup(self, vm_ref, pg_name):
+        kwargs = {}
+        kwargs["vm_ref"] = vm_ref
+        kwargs["pg_name"] = pg_name
+        self.methods["wait_for_portgroup"] = kwargs
+
+    def monitor_events(self):
+        self.methods["monitor_events"] = {}
+
+    def is_connected(self):
+        self.methods["is_connected"] = {}
+
+    def stop(self):
+        self.methods["stop"] = {}
+
+    def pause(self):
+        self.methods["pause"] = {}
+
+    def reset(self):
+        self.methods = {}
+
+
+class MockCallback(network_driver_callback.NetworkDriverCallback):
+
+    def __init__(self):
+        self.events = []
+
+    def process_event(self, event):
+        self.events.append(event)
