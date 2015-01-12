c1ba7ead762a7492b5f2f5f36e5eef6978a837fd
Romil Gupta <romilg@hp.com>
2015-01-12 03:59:35 -0800

    Added cache.py with unit test
    This patch-set cover the caching framework for OVSvApp Agent.
    
    Change-Id: I8c21487d6f413e02b46edd0952be56a9d3b2cf32

--- networking_vsphere/utils/cache.py
+++ networking_vsphere/utils/cache.py
@@ -16,17 +16,21 @@
 
 class VCCache(object):
 
-    clusters_id_to_path = {}  # cluster moid to path
-    vm_to_cluster = {}  # vm uuid to cluster mor
-    vm_uuid_to_mor = {}  # vm uuid to vm mor
-    vm_moid_to_uuid = {}  # vm moid to uuid
-    vm_uuid_to_model = {}  # vm uuid to VM model object
+    cluster_id_to_path = {}
+    vm_to_cluster = {}
+    vm_uuid_to_mor = {}
+    vm_moid_to_uuid = {}
+    vm_uuid_to_model = {}
     cluster_switch_mapping = {}
-    vm_uuid_to_esx_hostname = {}  # vm uuid to esx hostname
+    vm_uuid_to_esx_hostname = {}
 
     @classmethod
     def get_esx_hostname_for_vm(cls, vm_uuid):
         return cls.vm_uuid_to_esx_hostname[vm_uuid]
+
+    @classmethod
+    def add_esx_hostname_for_vm(cls, vm_uuid, hostname):
+        cls.vm_uuid_to_esx_hostname[vm_uuid] = hostname
 
     @classmethod
     def get_cluster_mor_for_vm(cls, vm_uuid):
@@ -34,35 +38,21 @@ class VCCache(object):
 
     @classmethod
     def add_cluster_mor_for_vm(cls, vm_uuid, clus_mor):
-        # TODO(NAME): Implement a LRU cache
         if len(cls.vm_to_cluster) >= 1000:
             cls.vm_to_cluster.popitem()
         cls.vm_to_cluster[vm_uuid] = clus_mor
 
     @classmethod
     def get_cluster_path_for_id(cls, cluster_id):
-        return cls.clusters_id_to_path.get(cluster_id, None)
-
-    @classmethod
-    def get_cluster_id_for_path(cls, cluster_path):
-        cluster_id = None
-        for temp_id, temp_path in cls.clusters_id_to_path.iteritems():
-            if cluster_path == temp_path:
-                cluster_id = temp_id
-                break
-        return cluster_id
-
-    @classmethod
-    def add_esx_hostname_for_vm(cls, vm_uuid, hostname):
-        cls.vm_uuid_to_esx_hostname[vm_uuid] = hostname
+        return cls.cluster_id_to_path.get(cluster_id, None)
 
     @classmethod
     def add_path_for_cluster_id(cls, cluster_id, cluster_path):
-        cls.clusters_id_to_path[cluster_id] = cluster_path
+        cls.cluster_id_to_path[cluster_id] = cluster_path
 
     @classmethod
-    def remove_cluster_id(cls, cluster_id):
-        cls.clusters_id_to_path.pop(cluster_id, None)
+    def get_vm_mor_for_uuid(cls, uuid):
+        return cls.vm_uuid_to_mor.get(uuid)
 
     @classmethod
     def add_vm_mor_for_uuid(cls, uuid, vm_mor):
@@ -70,20 +60,24 @@ class VCCache(object):
         cls.vm_moid_to_uuid[vm_mor.value] = uuid
 
     @classmethod
-    def get_vmuuid_for_moid(cls, moid):
-        return cls.vm_moid_to_uuid.get(moid)
-
-    @classmethod
-    def get_vm_mor_for_uuid(cls, uuid):
-        return cls.vm_uuid_to_mor.get(uuid)
+    def get_vm_model_for_uuid(cls, uuid):
+        return cls.vm_uuid_to_model.get(uuid)
 
     @classmethod
     def add_vm_model_for_uuid(cls, uuid, vm):
         cls.vm_uuid_to_model[uuid] = vm
 
     @classmethod
-    def get_vm_model_for_uuid(cls, uuid):
-        return cls.vm_uuid_to_model.get(uuid)
+    def get_switch_for_cluster_path(cls, cluster_path):
+        return cls.cluster_switch_mapping.get(cluster_path)
+
+    @classmethod
+    def add_switch_for_cluster_path(cls, cluster_path, switch_name):
+        cls.cluster_switch_mapping[cluster_path] = switch_name
+
+    @classmethod
+    def get_vmuuid_for_moid(cls, moid):
+        return cls.vm_moid_to_uuid.get(moid)
 
     @classmethod
     def remove_vm_for_uuid(cls, uuid):
@@ -94,16 +88,21 @@ class VCCache(object):
         cls.vm_uuid_to_model.pop(uuid, None)
 
     @classmethod
-    def add_switch_for_cluster_path(cls, cluster_path, switch_name):
-        cls.cluster_switch_mapping[cluster_path] = switch_name
-
-    @classmethod
-    def get_switch_for_cluster_path(cls, cluster_path):
-        return cls.cluster_switch_mapping[cluster_path]
-
-    @classmethod
     def remove_cluster_path(cls, cluster_path):
         del cls.cluster_switch_mapping[cluster_path]
+
+    @classmethod
+    def remove_cluster_id(cls, cluster_id):
+        cls.cluster_id_to_path.pop(cluster_id, None)
+
+    @classmethod
+    def get_cluster_id_for_path(cls, cluster_path):
+        cluster_id = None
+        for temp_id, temp_path in cls.cluster_id_to_path.iteritems():
+            if cluster_path == temp_path:
+                cluster_id = temp_id
+                break
+        return cluster_id
 
     @classmethod
     def get_cluster_switch_mapping(cls):
@@ -111,7 +110,7 @@ class VCCache(object):
 
     @classmethod
     def reset(cls):
-        cls.clusters_id_to_path = {}
+        cls.cluster_id_to_path = {}
         cls.vm_to_cluster = {}
         cls.vm_uuid_to_mor = {}
         cls.vm_moid_to_uuid = {}
