fd18b072f748347a690ed9a407d7784140e4fb01
Romil Gupta <romilg@hp.com>
2015-02-18 03:56:50 -0800

    Devstack support for OVSvApp
    Change-Id: I2b0b9d7ed587814afdb353dbfbf322276b700b92

---
+++ devstack/README.rst
@@ -0,0 +1,72 @@
+======================
+ Enabling in Devstack
+======================
+
+1. Download DevStack
+
+2. Add this repo as an external repository for OVSvApp Solution::
+
+     > cat local.conf
+     [[local|localrc]]
+     enable_plugin networking-vsphere http://git.openstack.org/stackforge/networking-vsphere
+
+
+3. Add the following required flags in local.conf to enable the OVSvApp L2 Agent::
+
+     Provide IP address for vCenter.
+     OVSVAPP_VCENTER_IP=$vCenter_ip_address
+
+     Provide vCenter Credentials.
+     OVSVAPP_VCENTER_USERNAME=$vCenter_user_name
+     OVSVAPP_VCENTER_PASSWORD=$vCenter_password
+
+     Provide ESX host name or IP address where OVSvApp VM is hosted.
+     OVSVAPP_ESX_HOSTNAME=$esx_hostname
+
+     Provide Cluster to DVS/vDS mapping.
+     OVSVAPP_CLUSTER_DVS_MAPPING=
+     For Example:
+     OVSVAPP_CLUSTER_DVS_MAPPING=DatacenterName/host/ClusterName:vDSName
+
+     Provide Physical Bridge name.
+     OVSVAPP_PHYSICAL_BRIDGE=
+     For Example:
+     OVSVAPP_PHYSICAL_BRIDGE=br-ethx
+
+     Provide Physical Interface to add port to Physical Bridge.
+     OVSVAPP_PHYSICAL_INTERFACE=
+     For Example:
+     OVSVAPP_PHYSICAL_INTERFACE=ethx
+
+     Provide Physical Bridge Mappings.
+     OVSVAPP_BRIDGE_MAPPINGS=
+     For Example:
+     OVSVAPP_BRIDGE_MAPPINGS=physnet1:ethx
+
+     Provide Trunk Interface.
+     OVSVAPP_TRUNK_INTERFACE=
+     For Example:
+     OVSVAPP_TRUNK_INTERFACE=ethy
+
+     Provide Security Bridge Mapping.
+     OVSVAPP_SECURITY_BRIDGE_MAPPINGS=
+     For Example:
+     OVSVAPP_SECURITY_BRIDGE_MAPPING=br-sec:ethy
+
+     Kindly, refer the ovsvapp_agent.ini for other default config parameters.
+
+
+4.  Add the following required flags in local.conf to enable the OVSvApp Compute VCDriver::
+
+     VIRT_DRIVER=vsphere
+     VMWAREAPI_IP=$vCenter_ip_address
+     VMWAREAPI_USER=$vCenter_user_name
+     VMWAREAPI_PASSWORD=$vCenter_password
+     VMWAREAPI_CLUSTER=$cluster_name
+
+     kindly, refer the following link:- https://wiki.openstack.org/wiki/NovaVMware/DeveloperGuide
+
+
+5. Read the settings file for more details.
+
+6. run ``stack.sh``
---
+++ devstack/plugin.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+#
+# devstack/plugin.sh
+# Functions to control the configuration and operation of the OVSvApp solution
+# Dependencies:
+#
+# ``functions`` file
+# ``DEST`` must be defined
+# ``STACK_USER`` must be defined
+
+# ``stack.sh`` calls the entry points in this order:
+#
+# - install_networking_vsphere
+# - install_ovsvapp_agent_packages
+# - add_ovsvapp_config
+# - configure_ovsvapp_config
+# - setup_ovsvapp_bridges
+# - start_ovsvapp_agent
+# - cleanup_ovsvapp_bridges
+
+# Save trace setting
+XTRACE=$(set +o | grep xtrace)
+set +o xtrace
+
+source $TOP_DIR/lib/neutron_plugins/ovs_base
+
+# OVSvApp Networking-vSphere DIR.
+OVSVAPP_NETWORKING_DIR=$DEST/networking-vsphere
+
+# Nova VMwareVCDriver DIR
+NOVA_VCDRIVER=$NOVA_DIR/nova/virt/vmwareapi/
+
+# OVSvApp VCDriver file path
+OVSVAPP_VCDRIVER=$OVSVAPP_NETWORKING_DIR/networking_vsphere/nova/virt/vmwareapi/ovsvapp_vc_driver.py
+
+# Entry Points
+# ------------
+
+function configure_ovsvapp_compute_driver {
+    echo "Configuring Nova VCDriver for OVSvApp"
+    cp $OVSVAPP_VCDRIVER $NOVA_VCDRIVER
+    iniset $NOVA_CONF DEFAULT compute_driver "vmwareapi.ovsvapp_vc_driver.OVSvAppVCDriver"
+}
+function start_ovsvapp_agent {
+    OVSVAPP_AGENT_BINARY="$NEUTRON_BIN_DIR/neutron-ovsvapp-agent"
+    echo "Starting OVSvApp L2 Agent"
+    run_process ovsvapp-agent "python $OVSVAPP_AGENT_BINARY --config-file $NEUTRON_CONF --config-file /$OVSVAPP_CONF_FILE"
+}
+
+function cleanup_ovsvapp_bridges {
+    echo "Removing Bridges for OVSvApp L2 Agent"
+    sudo ovs-vsctl del-br $INTEGRATION_BRIDGE
+    sudo ovs-vsctl del-br $SECURITY_BRIDGE
+    sudo ovs-vsctl del-br $OVSVAPP_PHYSICAL_BRIDGE
+}
+
+function setup_ovsvapp_bridges {
+    echo "Adding Bridges for OVSvApp L2 Agent"
+    sudo ovs-vsctl --no-wait -- --may-exist add-br $INTEGRATION_BRIDGE
+    sudo ovs-vsctl --no-wait -- --may-exist add-br $SECURITY_BRIDGE
+    sudo ovs-vsctl --no-wait -- --may-exist add-br $OVSVAPP_PHYSICAL_BRIDGE
+    sudo ovs-vsctl --no-wait -- --may-exist add-port $OVSVAPP_PHYSICAL_BRIDGE $OVSVAPP_PHYSICAL_INTERFACE
+    sudo ovs-vsctl --no-wait -- --may-exist add-port $SECURITY_BRIDGE $OVSVAPP_TRUNK_INTERFACE
+}
+
+function configure_ovsvapp_config {
+    echo "Configuring ovsvapp_agent.ini for OVSvApp"
+    iniset /$OVSVAPP_CONF_FILE vmware vcenter_ip $OVSVAPP_VCENTER_IP
+    iniset /$OVSVAPP_CONF_FILE vmware vcenter_username $OVSVAPP_VCENTER_USERNAME
+    iniset /$OVSVAPP_CONF_FILE vmware vcenter_password $OVSVAPP_VCENTER_PASSWORD
+    iniset /$OVSVAPP_CONF_FILE vmware wsdl_location $OVSVAPP_WSDL_LOCATION
+    iniset /$OVSVAPP_CONF_FILE vmware cluster_dvs_mapping $OVSVAPP_CLUSTER_DVS_MAPPING
+    iniset /$OVSVAPP_CONF_FILE vmware esx_hostname $OVSVAPP_ESX_HOSTNAME
+    iniset /$OVSVAPP_CONF_FILE ovsvapp bridge_mappings $OVSVAPP_BRIDGE_MAPPINGS
+    iniset /$OVSVAPP_CONF_FILE securitygroup security_bridge_mapping $OVSVAPP_SECURITY_BRIDGE_MAPPINGS
+}
+
+function add_ovsvapp_config {
+    OVSVAPP_CONF_PATH=etc/neutron/plugins/ovsvapp
+    OVSVAPP_CONF_FILENAME=ovsvapp_agent.ini
+    mkdir -p /$OVSVAPP_CONF_PATH
+    OVSVAPP_CONF_FILE=$OVSVAPP_CONF_PATH/$OVSVAPP_CONF_FILENAME
+    echo "Adding configuration file for OVSvApp Agent"
+    cp $OVSVAPP_NETWORKING_DIR/$OVSVAPP_CONF_FILE /$OVSVAPP_CONF_FILE
+}
+
+function install_ovsvapp_agent_packages {
+    echo "Installing Openvswitch"
+    _neutron_ovs_base_install_agent_packages
+}
+
+function install_networking_vsphere {
+    echo "Installing the Networking-vSphere for OVSvApp"
+    setup_develop $OVSVAPP_NETWORKING_DIR
+}
+
+# main loop
+if is_service_enabled ovsvapp-server; then
+    if [[ "$1" == "source" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        install_networking_vsphere
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "post-extra" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # no-op
+        :
+    fi
+fi
+
+if is_service_enabled ovsvapp-agent; then
+    if [[ "$1" == "source" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        install_networking_vsphere
+        install_ovsvapp_agent_packages
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        add_ovsvapp_config
+        configure_ovsvapp_config
+        setup_ovsvapp_bridges
+        start_ovsvapp_agent
+    elif [[ "$1" == "stack" && "$2" == "post-extra" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+       cleanup_ovsvapp_bridges
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        cleanup_ovsvapp_bridges
+    fi
+fi
+
+if is_service_enabled ovsvapp-compute; then
+    if [[ "$1" == "source" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        install_networking_vsphere
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        configure_ovsvapp_compute_driver
+    elif [[ "$1" == "stack" && "$2" == "post-extra" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # no-op
+        :
+    fi
+fi
+
+# Restore xtrace
+$XTRACE
+
+# Tell emacs to use shell-script-mode
+## Local variables:
+## mode: shell-script
+## End:
