4cae99329401aaf844707660f4499d5f3aaa21f0
Romil Gupta <romilg@hp.com>
2015-03-30 04:43:50 -0700

    Server side Implementation - VXLAN
    This patch has the following:
    - Model class for the VNI to Local VLAN allocations.
    - DB module to handle the various CRUD operations to the table.
    - OVSvApp driver changes to initialize the table with default VNI mappings.
    - A new RPC call to inform the ovsvapp agent about vxlan port group deletion.
       - The status of this RPC call is used as the basis for reclaiming
         the allocationed local VLAN.
    
    Co-Authored-By: Sudhakar Babu Gariganti <sudhakar-babu.gariganti@hp.com>
    
    Change-Id: I01bb32190131f0803930652561871b8d4aaa589c

---
+++ networking_vsphere/db/ovsvapp_db.py
@@ -0,0 +1,237 @@
+# Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log
+import sqlalchemy.orm.exc as sa_exc
+
+from neutron.db import api as db_api
+
+from networking_vsphere.db import ovsvapp_models as models
+
+LOG = log.getLogger(__name__)
+
+
+def _generate_vcenter_cluster_allocations(session, vcenter, cluster):
+    bulk_size = 100
+    allocs = []
+    lvid_min = 1
+    lvid_max = 4095
+
+    for lvid in xrange(lvid_min, lvid_max):
+        allocs.append({'vcenter_id': vcenter,
+                       'cluster_id': cluster,
+                       'lvid': lvid})
+    if allocs:
+        chuncked_allocs = (allocs[i:i + bulk_size] for i in
+                           range(0, len(allocs), bulk_size))
+        for bulk in chuncked_allocs:
+            session.execute(models.ClusterVNIAllocations.
+                            __table__.insert(), bulk)
+    LOG.info(_("Finished initializing local vlans for cluster %(cluster)s "
+               "of vCenter %(vcenter)s."), {'cluster': cluster,
+                                            'vcenter': vcenter})
+
+
+def _initialize_lvids_for_cluster(port_info):
+    vcenter = port_info['vcenter_id']
+    cluster = port_info['cluster_id']
+    session = db_api.get_session()
+    with session.begin(subtransactions=True):
+        try:
+            (session.query(models.ClusterVNIAllocations).
+             with_lockmode('update')).all()
+            query = session.query(models.ClusterVNIAllocations)
+            existing_allocations = query.filter(
+                models.ClusterVNIAllocations.vcenter_id == vcenter,
+                models.ClusterVNIAllocations.cluster_id == cluster
+            ).all()
+            if not existing_allocations:
+                _generate_vcenter_cluster_allocations(
+                    session, vcenter, cluster)
+            return True
+        except Exception:
+            LOG.exception(_("Exception while initializing VNI allocations for "
+                            "clusters %(cluster)s of vCenter %(vcenter)s."),
+                          {'cluster': cluster,
+                           'vcenter': vcenter})
+            return False
+
+
+def _try_to_obtain_local_vlan(session, port_info, assign):
+    lvid = None
+    res_keys = ['vcenter_id', 'cluster_id', 'network_id']
+    res = dict((k, port_info[k]) for k in res_keys)
+    try:
+        allocation = (session.query(models.ClusterVNIAllocations).filter(
+            models.ClusterVNIAllocations.vcenter_id == res['vcenter_id'],
+            models.ClusterVNIAllocations.cluster_id == res['cluster_id'],
+            models.ClusterVNIAllocations.network_id == res['network_id']
+        ).one())
+        lvid = allocation.lvid
+        if assign:
+            count = allocation.network_port_count + 1
+            allocation.update({'network_port_count': count})
+    except sa_exc.NoResultFound:
+        if not assign:
+            raise Exception()
+        try:
+            allocation = session.query(models.ClusterVNIAllocations).filter(
+                models.ClusterVNIAllocations.vcenter_id == res['vcenter_id'],
+                models.ClusterVNIAllocations.cluster_id == res['cluster_id'],
+                models.ClusterVNIAllocations.allocated == 0
+            ).first()
+            if allocation:
+                lvid = allocation.lvid
+                allocation.update({'network_id': res['network_id'],
+                                   'allocated': True,
+                                   'network_port_count': 1})
+                LOG.info(_("Assigned local vlan %(lvid)s for the network "
+                           "%(network)s on the cluster %(cluster)s."),
+                         {'network': port_info['network_id'],
+                          'cluster': port_info['cluster_id'],
+                          'lvid': lvid})
+            else:
+                LOG.error(_("All available VLANs are used up in the cluster "
+                            "%(cluster)s of vCenter %(vcenter)s."),
+                          {'cluster': res['cluster_id'],
+                           'vcenter': res['vcenter_id']})
+        except Exception as e:
+            LOG.exception(_("Unable to obtain local vlan id %s."), e)
+    return lvid
+
+
+def get_local_vlan(port_info, assign=True):
+    lvid = None
+    session = db_api.get_session()
+    res_keys = ['vcenter_id', 'cluster_id', 'network_id']
+    res = dict((k, port_info[k]) for k in res_keys)
+    with session.begin(subtransactions=True):
+        try:
+            if not assign:
+                lvid = _try_to_obtain_local_vlan(session, port_info, assign)
+                return lvid
+            query = session.query(models.ClusterVNIAllocations)
+            # Lock all the rows in the table corresponding to the vCenter
+            # and cluster.
+            cluster_rows = query.filter(
+                (models.ClusterVNIAllocations.vcenter_id == res['vcenter_id']),
+                (models.ClusterVNIAllocations.cluster_id == res['cluster_id'])
+            ).with_lockmode('update').all()
+            if cluster_rows:
+                lvid = _try_to_obtain_local_vlan(session, port_info, assign)
+                return lvid
+            else:
+                LOG.info(_("Local VLAN rows not provisioned for the "
+                           "cluster %(cluster)s of vCenter %(vcenter)s. "
+                           "Going to provision."),
+                         {'cluster': res['cluster_id'],
+                          'vcenter': res['vcenter_id']})
+        except Exception:
+            LOG.exception(_("Error retrieving a local vlan for network "
+                            "%(network)s for %(port)s."),
+                          {'network': port_info['network_id'],
+                           'port': port_info['port_id']})
+            return
+    status = _initialize_lvids_for_cluster(res)
+    if status:
+        with session.begin(subtransactions=True):
+            lvid = _try_to_obtain_local_vlan(session, port_info, assign)
+    else:
+        LOG.error(_("Local VLAN rows not provisioned for the "
+                    "cluster %(cluster)s of vCenter %(vcenter)s."),
+                  {'cluster': res['cluster_id'],
+                   'vcenter': res['vcenter_id']})
+    return lvid
+
+
+def check_to_reclaim_local_vlan(port_info):
+    lvid = -1
+    session = db_api.get_session()
+    with session.begin(subtransactions=True):
+        res_keys = ['vcenter_id', 'cluster_id', 'network_id']
+        res = dict((k, port_info[k]) for k in res_keys)
+        try:
+            query = session.query(models.ClusterVNIAllocations)
+            allocation = (query.filter(
+                models.ClusterVNIAllocations.vcenter_id == res['vcenter_id'],
+                models.ClusterVNIAllocations.cluster_id == res['cluster_id'],
+                models.ClusterVNIAllocations.network_id == res['network_id']
+            ).with_lockmode('update').one())
+            count = allocation.network_port_count
+            if count >= 1:
+                count -= 1
+                allocation.update({'network_port_count': count})
+            if count == 0:
+                lvid = allocation.lvid
+        except sa_exc.NoResultFound:
+            # Nothing to do, may be another controller cleared the record
+            # We will just log and return back status as False.
+            LOG.debug("Network %(network)s is already de-allocated for "
+                      "cluster %(cluster)s.",
+                      {'network': port_info['network_id'],
+                       'cluster': port_info['cluster_id']})
+    return lvid
+
+
+def release_local_vlan(net_info):
+    session = db_api.get_session()
+    with session.begin(subtransactions=True):
+        res_keys = ['vcenter_id', 'cluster_id', 'network_id']
+        res = dict((k, net_info[k]) for k in res_keys)
+        try:
+            query = session.query(models.ClusterVNIAllocations)
+            allocation = (query.filter(
+                models.ClusterVNIAllocations.vcenter_id == res['vcenter_id'],
+                models.ClusterVNIAllocations.cluster_id == res['cluster_id'],
+                models.ClusterVNIAllocations.network_id == res['network_id']
+            ).with_lockmode('update').one())
+            if allocation.network_port_count == 0:
+                allocation.update({'network_id': None,
+                                   'allocated': False,
+                                   'network_port_count': 0})
+            else:
+                LOG.info(_("Unable to release local vlan for network_id %s "
+                           "because ports are available on network."),
+                         res['network_id'])
+        except sa_exc.NoResultFound:
+            # Nothing to do, may be another controller cleared the record
+            # We will just log and return.
+            LOG.error(_("Network %(network)s is already de-allocated for "
+                        "cluster %(cluster)s."),
+                      {'network': net_info['network_id'],
+                       'cluster': net_info['cluster_id']})
+
+
+def get_stale_local_vlans_for_network(network_id):
+    session = db_api.get_session()
+    vcenter_clusters = None
+    with session.begin(subtransactions=True):
+        try:
+            query = session.query(models.ClusterVNIAllocations)
+            allocations = (query.filter(
+                models.ClusterVNIAllocations.network_id == network_id
+            ).all())
+            if allocations:
+                vcenter_clusters = []
+                for alloc in allocations:
+                    vcenter_clusters.append((alloc.vcenter_id,
+                                             alloc.cluster_id,
+                                             alloc.lvid))
+        except Exception:
+            # Nothing to do, port-deletions have properly cleaned up
+            # the records. We will just log and return back empty list.
+            LOG.debug("Network %s is already cleaned up from "
+                      "VNI allocations table.", network_id)
+    return vcenter_clusters
--- networking_vsphere/ml2/ovsvapp_driver.py
+++ networking_vsphere/ml2/ovsvapp_driver.py
@@ -14,12 +14,21 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import eventlet
+eventlet.monkey_patch()
 from oslo_log import log
+from oslo_utils import timeutils
 
 from neutron.common import rpc as n_rpc
 from neutron.common import topics
+from neutron import context as neutron_context
+from neutron.extensions import portbindings
+from neutron import manager
+from neutron.plugins.common import constants as p_const
+from neutron.plugins.ml2 import driver_api as api
 
 from networking_vsphere.common import constants
+from networking_vsphere.db import ovsvapp_db
 from networking_vsphere.ml2 import ovsvapp_rpc
 
 LOG = log.getLogger(__name__)
@@ -33,7 +42,22 @@ class OVSvAppAgentDriver(object):
     """
 
     def initialize(self):
+        self.context = neutron_context.get_admin_context()
         self._start_rpc_listeners()
+        self._plugin = None
+        self._pool = None
+
+    @property
+    def plugin(self):
+        if self._plugin is None:
+            self._plugin = manager.NeutronManager.get_plugin()
+        return self._plugin
+
+    @property
+    def threadpool(self):
+        if self._pool is None:
+            self._pool = eventlet.GreenPool(2)
+        return self._pool
 
     def _start_rpc_listeners(self):
         self.notifier = ovsvapp_rpc.OVSvAppAgentNotifyAPI(topics.AGENT)
@@ -42,3 +66,106 @@ class OVSvAppAgentDriver(object):
         self.conn = n_rpc.create_connection(new=True)
         self.conn.create_consumer(self.topic, self.endpoints, fanout=False)
         return self.conn.consume_in_threads()
+
+    def _get_ovsvapp_agent_from_cluster(self, context, cluster_id):
+        chosen_agent = None
+        agents = self.plugin.get_agents(
+            context,
+            filters={'agent_type': [constants.AGENT_TYPE_OVSVAPP]})
+        chosen_agent = agents[0]
+        recent_time = chosen_agent['heartbeat_timestamp']
+        agents = agents[1:]
+        for agent in agents:
+            agent_cluster_id = agent['configurations'].get('cluster_id')
+            if cluster_id != agent_cluster_id:
+                continue
+            delta = timeutils.delta_seconds(recent_time,
+                                            agent['heartbeat_timestamp'])
+            if delta > 0:
+                recent_time = agent['heartbeat_timestamp']
+                chosen_agent = agent
+        return chosen_agent
+
+    def _notify_agent(self, network_info):
+        host = None
+        if 'host' in network_info:
+            host = network_info['host']
+        else:
+            cluster = network_info['cluster_id']
+            agent = self._get_ovsvapp_agent_from_cluster(self.context,
+                                                         cluster)
+            if agent and 'host' in agent:
+                host = agent['host']
+            else:
+                LOG.error(_("Failed to find OVSvApp Agent with host "
+                            "%(host)s while releasing network allocations "
+                            "for %(cluster)s in vCenter %(vcenter)s."),
+                          {'host': host,
+                           'vcenter': network_info['vcenter_id'],
+                           'cluster': cluster})
+                return
+        try:
+            self.notifier.device_delete(self.context, network_info, host)
+        except Exception:
+            LOG.exception(_("Failed to notify agent to delete port group "))
+
+    def delete_port_postcommit(self, context):
+        """Delete port non-database commit event."""
+        port = context.current
+        if port and port['device_owner'].startswith('compute'):
+            segment = context.bound_segment
+            if segment and segment[api.NETWORK_TYPE] == p_const.TYPE_VXLAN:
+                host = port[portbindings.HOST_ID]
+                vcenter = None
+                cluster = None
+                agents = self.plugin.get_agents(
+                    self.context,
+                    filters={'agent_type': [constants.AGENT_TYPE_OVSVAPP],
+                             'host': [host]})
+                agent = agents[0]
+                vcenter = agent['configurations']['vcenter']
+                cluster = agent['configurations']['cluster_id']
+                net_info = {'vcenter_id': vcenter,
+                            'cluster_id': cluster,
+                            'network_id': port['network_id'],
+                            'segmentation_id': segment[api.SEGMENTATION_ID],
+                            'host': host}
+                try:
+                    lvid = ovsvapp_db.check_to_reclaim_local_vlan(net_info)
+                    if lvid >= 1:
+                        net_info.update({'lvid': lvid})
+                        LOG.debug("Spawning thread for releasing network "
+                                  "VNI allocations for %s.", net_info)
+                        self.threadpool.spawn_n(self._notify_agent, net_info)
+                except Exception:
+                    LOG.exception(_("Failed to check for reclaiming "
+                                    "local vlan."))
+
+    def delete_network_postcommit(self, context):
+        try:
+            network = context.current
+            segments = context.network_segments
+            vxlan_segments = []
+            if segments:
+                for segment in segments:
+                    if segment[api.NETWORK_TYPE] == p_const.TYPE_VXLAN:
+                        vxlan_segments.append(segment)
+            if not vxlan_segments:
+                return
+            stale_entries = ovsvapp_db.get_stale_local_vlans_for_network(
+                network['id'])
+            if stale_entries:
+                for (vcenter, cluster, lvid) in stale_entries:
+                    network_info = {'vcenter_id': vcenter,
+                                    'cluster_id': cluster,
+                                    'lvid': lvid,
+                                    'network_id': network['id']}
+                    if len(vxlan_segments) == 1:
+                        seg_id = vxlan_segments[0][api.SEGMENTATION_ID]
+                        network_info.update({'segmentation_id': seg_id})
+                    LOG.debug("Spawning thread for releasing network "
+                              "VNI allocations for %s.", network_info)
+                    self.threadpool.spawn_n(self._notify_agent, network_info)
+        except Exception:
+            LOG.exception(_("Failed to check for stale "
+                            "local vlan allocations."))
