7ac1dfe03e1820971b6d02aac7b6a5a48ef36769
armando-migliaccio <armamig@gmail.com>
2015-04-06 19:11:12 -0700

    Sentinel change to valdiate UT post Neutron UT-reorg
    Change-Id: Ic56121315c5b7a0980bd224e0722c9ca9be05b7b

--- networking_vsphere/tests/unit/ml2/test_ovsvapp_driver.py
+++ networking_vsphere/tests/unit/ml2/test_ovsvapp_driver.py
@@ -19,6 +19,7 @@ from neutron.extensions import portbindings
 from neutron.plugins.ml2 import driver_api as api
 from neutron.plugins.ml2.drivers.ovsvapp import mech_driver
 from neutron.tests.unit.ml2 import _test_mech_agent as base
+
 # TODO(romilg): Revisit to minimize dependency on ML2 tests.
 
 from networking_vsphere.common import constants
