c156ca8475a01ef9ca9b6519f3cf54f4db9c5a47
abhishek60014726 <abhishek.g-m@hp.com>
2015-04-24 06:17:51 -0700

    manager.py and tempest files
    Change-Id: Ibc69b5db0453c29fee51890ca0b2ae200749dcb4

---
+++ networking_vsphere/tests/tempest/services/compute/json/certificates_client.py
@@ -0,0 +1,37 @@
+# Copyright 2013 IBM Corp
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+
+from networking_vsphere.tests.tempest.api_schema.response.compute.v2_1 import certificates as schema
+from networking_vsphere.tests.tempest.common import service_client
+
+
+class CertificatesClientJSON(service_client.ServiceClient):
+
+    def get_certificate(self, id):
+        url = "os-certificates/%s" % (id)
+        resp, body = self.get(url)
+        body = json.loads(body)
+        self.validate_response(schema.get_certificate, resp, body)
+        return service_client.ResponseBody(resp, body['certificate'])
+
+    def create_certificate(self):
+        """create certificates."""
+        url = "os-certificates"
+        resp, body = self.post(url, None)
+        body = json.loads(body)
+        self.validate_response(schema.create_certificate, resp, body)
+        return service_client.ResponseBody(resp, body['certificate'])
---
+++ networking_vsphere/tests/tempest/services/compute/json/limits_client.py
@@ -0,0 +1,37 @@
+# Copyright 2012 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+
+from networking_vsphere.tests.tempest.api_schema.response.compute.v2_1 import limits as schema
+from networking_vsphere.tests.tempest.common import service_client
+
+
+class LimitsClientJSON(service_client.ServiceClient):
+
+    def get_absolute_limits(self):
+        resp, body = self.get("limits")
+        body = json.loads(body)
+        self.validate_response(schema.get_limit, resp, body)
+        return service_client.ResponseBody(resp, body['limits']['absolute'])
+
+    def get_specific_absolute_limit(self, absolute_limit):
+        resp, body = self.get("limits")
+        body = json.loads(body)
+        self.validate_response(schema.get_limit, resp, body)
+        if absolute_limit not in body['limits']['absolute']:
+            return None
+        else:
+            return body['limits']['absolute'][absolute_limit]
