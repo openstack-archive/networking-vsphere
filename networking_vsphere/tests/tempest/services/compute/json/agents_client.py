4b79642d624dba2176acb08c9578dea5d92afd98
abhishek60014726 <abhishek.g-m@hp.com>
2015-04-24 06:17:51 -0700

    manager.py and tempest files
    Change-Id: Ibc69b5db0453c29fee51890ca0b2ae200749dcb4

---
+++ networking_vsphere/tests/tempest/services/compute/json/agents_client.py
@@ -0,0 +1,55 @@
+# Copyright 2014 NEC Corporation.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+import urllib
+
+from networking_vsphere.tests.tempest.api_schema.response.compute.v2_1 import agents as schema
+from networking_vsphere.tests.tempest.common import service_client
+
+
+class AgentsClientJSON(service_client.ServiceClient):
+    """
+    Tests Agents API
+    """
+
+    def list_agents(self, params=None):
+        """List all agent builds."""
+        url = 'os-agents'
+        if params:
+            url += '?%s' % urllib.urlencode(params)
+        resp, body = self.get(url)
+        body = json.loads(body)
+        self.validate_response(schema.list_agents, resp, body)
+        return service_client.ResponseBodyList(resp, body['agents'])
+
+    def create_agent(self, **kwargs):
+        """Create an agent build."""
+        post_body = json.dumps({'agent': kwargs})
+        resp, body = self.post('os-agents', post_body)
+        body = json.loads(body)
+        self.validate_response(schema.create_agent, resp, body)
+        return service_client.ResponseBody(resp, body['agent'])
+
+    def delete_agent(self, agent_id):
+        """Delete an existing agent build."""
+        resp, body = self.delete("os-agents/%s" % str(agent_id))
+        self.validate_response(schema.delete_agent, resp, body)
+        return service_client.ResponseBody(resp, body)
+
+    def update_agent(self, agent_id, **kwargs):
+        """Update an agent build."""
+        put_body = json.dumps({'para': kwargs})
+        resp, body = self.put('os-agents/%s' % str(agent_id), put_body)
+        return service_client.ResponseBody(resp, self._parse_resp(body))
---
+++ networking_vsphere/tests/tempest/services/compute/json/migrations_client.py
@@ -0,0 +1,34 @@
+# Copyright 2014 NEC Corporation.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+import urllib
+
+from networking_vsphere.tests.tempest.api_schema.response.compute import migrations as schema
+from networking_vsphere.tests.tempest.common import service_client
+
+
+class MigrationsClientJSON(service_client.ServiceClient):
+
+    def list_migrations(self, params=None):
+        """Lists all migrations."""
+
+        url = 'os-migrations'
+        if params:
+            url += '?%s' % urllib.urlencode(params)
+
+        resp, body = self.get(url)
+        body = json.loads(body)
+        self.validate_response(schema.list_migrations, resp, body)
+        return service_client.ResponseBodyList(resp, body['migrations'])
