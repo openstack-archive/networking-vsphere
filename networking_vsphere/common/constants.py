54b3f75fb7101b055c9f3c8baaf2438d953c340f
Romil Gupta <romilg@hp.com>
2015-08-12 00:05:45 -0700

    Updated ovsvapp_agent.ini location
    (cherry picked from commit 7e682a21b21b6be5b1cea224dd80ab3088655073)
    
    Closes-Bug: #1485966
    Change-Id: I51b082a9c36c3cf7d2ea2d829306ec2d892eccf3

--- networking_vsphere/common/constants.py
+++ networking_vsphere/common/constants.py
@@ -14,7 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-AGENT_TYPE_OVSVAPP = "OVSvApp L2 Agent"
+AGENT_TYPE_OVSVAPP = "OVSvApp Agent"
 OVSVAPP = 'ovsvapp'
 DEVICE = 'device'
 
