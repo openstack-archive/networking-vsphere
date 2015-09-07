7cde89f0d4ff1575619bad2a328801bb99948378
Romil Gupta <romilg@hp.com>
2015-09-07 02:46:25 -0700

    Fixes for OVSvApp monitoring
    This patch set fixes the following issues:
    
    1. Add the entry point for neutron-ovsvapp-agent-monitor.
    2. Update the binary file for ovsvapp monitoring.
    3. Enable logging for ovsvapp monitor.
    4. Fixes ovsvapp-agent-monitor.sh script.
    5. Fixes monitor.py to take the ovs_monitor path dynamically.
    
    Change-Id: Idaf7e76bb9c8d3b08ac708d3146a801f51d71883

--- networking_vsphere/monitor/ovsvapp-agent-monitor.sh
+++ networking_vsphere/monitor/ovsvapp-agent-monitor.sh
@@ -27,26 +27,22 @@ fi
 
 # Case when openvswitch-switch process is not responding.
 # Check for ovs-vswitch process.
-    ovs-ofctl show br-int
+    sudo ovs-ofctl show br-int
     if [ $? -ne 0 ]; then
        echo $stopped_msg >> $logfile ||true
-       stop openvswitch-switch || true
-       start openvswitch-switch || true
+       sudo stop openvswitch-switch || true
+       sudo start openvswitch-switch || true
        echo $restarted_msg >> $logfile ||true
-       stop neutron-ovsvapp-agent || true
-       start neutron-ovsvapp-agent || true
        echo $broken_msg >> $logfile
     fi
 sleep 1
 # Check for ovsdb-server process.
-    ovs-vsctl show
+    sudo ovs-vsctl show
     if [ $? -ne 0 ]; then
        echo $stopped_msg >> $logfile || true
-       stop openvswitch-switch || true
-       start openvswitch-switch || true
+       sudo stop openvswitch-switch || true
+       sudo start openvswitch-switch || true
        echo $restarted_msg >> $logfile || true
-       stop neutron-ovsvapp-agent || true
-       start neutron-ovsvapp-agent || true
        echo $broken_msg >> $logfile
     fi
 sleep 2
