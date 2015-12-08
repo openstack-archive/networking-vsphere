37457346d014187b8295131522d94d7f7d016462
prabhm <prabhu.m4@hp.com>
2015-12-07 21:57:16 -0800

    Removing Hard coded values of interface & database
    1) Added neutron database name and trunk interface to tempest.conf
       as Variable rather than Hard coded values in test
    
    Change-Id: I3c4ae18b20ccd031eb70e4312bc74e36678cb880
    Closes-Bug: 1523761

--- networking_vsphere/tests/tempest/config.py
+++ networking_vsphere/tests/tempest/config.py
@@ -24,7 +24,8 @@ VCENTER_OPTS = [
     cfg.StrOpt('vcenter_username',
                help="Username to login to vCenter "),
     cfg.StrOpt('vcenter_password',
-               help="Password to login to vCenter "),
+               help="Password to login to vCenter ",
+               secret=True),
     cfg.StrOpt('tenant_network_type',
                default="vlan",
                help="tenant network type is vlan or vxlan"),
@@ -35,7 +36,12 @@ VCENTER_OPTS = [
     cfg.StrOpt('vapp_username',
                help="Username to login to OVSvApp "),
     cfg.StrOpt('vapp_password',
-               help="Password to login to OVSvApp")
+               help="Password to login to OVSvApp",
+               secret=True),
+    cfg.StrOpt('brigde_inferace_trunk',
+               help="Brigde inferace of trunk dvs")
+    cfg.StrOpt('neutron_database_name',
+               help="Neutron database name as in mysql table")
 ]
 
 
