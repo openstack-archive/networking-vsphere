b154202c276a0430eda92144cb6878a3a1304010
Abhishek G.M <abhishek.gm@hpe.com>
2015-12-21 03:50:54 -0800

    Basic test for agent monitoring on ESX host
    1) Boot a vm
    2) Check for connectivity of booted vm
    3) Kill the process of ovsvapp agent
    4) Stop openvswitch service
    5) Check ovsvapp vm is poweredoff
    6) Check host entered into maintenance mode
    7) Exit maintanance mode
    8) Power on the ovsvapp vm
    9) Run unstack.sh adn stack.sh file
    
    Change-Id: Ic1f9e0fa1ac3203ac1ac039d3e3dc95d24186ba2

--- networking_vsphere/tests/scenario/manager.py
+++ networking_vsphere/tests/scenario/manager.py
@@ -136,6 +136,27 @@ class ESXNetworksTestJSON(base.BaseAdminNetworkTest,
         content = connection.RetrieveContent()
         return content
 
+    def _get_vm_power_on(self, vm_ip):
+        content = self._create_connection()
+        vm_name = self.get_obj(content, [vim.VirtualMachine],
+                               vm_ip)
+        power = vm_name.PowerOn()
+        return power
+
+    def _get_host_uuid(self, host):
+        content = self._create_connection()
+        uuid = self.get_obj(content, [vim.HostSystem],
+                            host)
+        host_uuid = uuid.hardware.systemInfo.uuid
+        return host_uuid
+
+    def _get_vm_power_status(self, host):
+        content = self._create_connection()
+        vm_name = self.get_obj(content, [vim.VirtualMachine],
+                               host)
+        power_state = vm_name.runtime.powerState
+        return power_state
+
     def _get_portgroups(self):
         trunk_dvswitch_name = cfg.CONF.VCENTER.trunk_dvswitch_name
         content = self._create_connection()
