ff2adf41c743c4a9ddf910939be8178d4dfd44f6
Abhishek G.M <abhishek.gm@hpe.com>
2015-12-21 03:50:54 -0800

    Basic test for agent monitoring on ESX host
    1) Boot a vm
    2) Check for connectivity of booted vm
    3) Kill the process of ovsvapp agent
    4) Stop openvswitch service
    5) Check ovsvapp vm is poweredoff
    6) Check host entered into maintenance mode
    7) Exit maintanance mode
    8) Power on the ovsvapp vm
    9) Run unstack.sh adn stack.sh file
    
    Change-Id: Ic1f9e0fa1ac3203ac1ac039d3e3dc95d24186ba2

--- networking_vsphere/tests/tempest/config.py
+++ networking_vsphere/tests/tempest/config.py
@@ -38,6 +38,13 @@ VCENTER_OPTS = [
     cfg.StrOpt('vapp_password',
                help="Password to login to OVSvApp",
                secret=True)
+               help="Password to login to OVSvApp"),
+    cfg.StrOpt('host_username',
+               help="The username of the host"),
+    cfg.StrOpt('agent_down_time',
+               help="Time taken to turn off the OVSvApp "),
+    cfg.StrOpt('devstack',
+               help="Devstack is used for test")
 ]
 
 
