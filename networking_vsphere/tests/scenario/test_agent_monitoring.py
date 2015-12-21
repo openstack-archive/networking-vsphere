fd256927e50a101c7d68074f0c333560ace5c549
Abhishek G.M <abhishek.gm@hpe.com>
2015-12-21 04:08:04 -0800

    Verify Datapath status in the status.json file
    Verify status after ovsvapp deployment
    
    Change-Id: I7a28bf33bf2f508fe170ffca4b8cc2358e4dd884

---
+++ networking_vsphere/tests/scenario/test_agent_monitoring.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2016 Hewlett-Packard Enterprise Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import jsonutils
+import subprocess
+
+from networking_vsphere.tests.scenario import manager
+
+from oslo_config import cfg
+
+
+CONF = cfg.CONF
+
+
+class OVSVAPPTestJSON(manager.ESXNetworksTestJSON):
+
+    def test_datapath_status_after_ovsvapp_deployment(self):
+        agent_list = self.admin_client.list_agents(agent_type='OVSvApp Agent',
+                                                   alive="True")
+        devstack_status = CONF.VCENTER.devstack
+        if devstack_status == 'yes':
+            for i in range(2):
+                config = agent_list['agents'][i]['configurations']
+                ovsvapp_ip = config['monitoring_ip']
+                vapp_username = CONF.VCENTER.vapp_username
+                HOST = vapp_username + "@" + ovsvapp_ip
+                path = '/opt/stack/logs/status.json'
+                ssh = subprocess.Popen(['ssh', "%s" % HOST, 'cat', path],
+                                       stdout=subprocess.PIPE)
+                output = ssh.stdout.readlines()
+                output_loads = jsonutils.loads(output[0])
+                output_dumps = jsonutils.dumps(output_loads['ovs'])
+                quote = output_dumps[1:-1]
+                self.assertEqual(quote, 'OK')
+        else:
+            for i in range(2):
+                config = agent_list['agents'][i]['configurations']
+                ovsvapp_ip = config['monitoring_ip']
+                vapp_username = CONF.VCENTER.vapp_username
+                HOST = vapp_username + "@" + ovsvapp_ip
+                path = '/var/log/neutron/ovsvapp-agent/status.json'
+                ssh = subprocess.Popen(['ssh', "%s" % HOST, 'cat', path],
+                                       stdout=subprocess.PIPE)
+                output = ssh.stdout.readlines()
+                output_loads = jsonutils.loads(output[0])
+                output_dumps = jsonutils.dumps(output_loads['ovs'])
+                quote = output_dumps[1:-1]
+                self.assertEqual(quote, 'OK')
