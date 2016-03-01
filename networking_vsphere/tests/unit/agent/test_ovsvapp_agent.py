0a54edd1c429717634df081f85c0790157ca97af
M Arun Kumar <madheven-arun.kumar@hpe.com>
2016-02-29 23:47:04 -0800

    Instance VMs are not getting IP address due to flows not being processed by agent
    Change-Id: I55795a3be11183be2add35c407813f68fc8609eb

--- networking_vsphere/tests/unit/agent/test_ovsvapp_agent.py
+++ networking_vsphere/tests/unit/agent/test_ovsvapp_agent.py
@@ -51,7 +51,7 @@ FAKE_SG = {'fake_sg': 'fake_sg_rule'}
 FAKE_SG_RULES = {FAKE_PORT_1: {'security_group_source_groups': ['fake_rule_1',
                                                                 'fake_rule_2',
                                                                 'fake_rule_3'],
-                               'security_group_rules': [
+                               'sg_provider_rules': [
                                {'ethertype': 'IPv4',
                                 'direction': 'egress',
                                 'source_port_range_min': 67,
@@ -1364,7 +1364,7 @@ class TestOVSvAppAgent(base.TestCase):
             self.agent.device_create(FAKE_CONTEXT,
                                      device=DEVICE,
                                      ports=ports,
-                                     sg_rules=mock.MagicMock())
+                                     sg_rules=FAKE_SG_RULES)
             self.assertTrue(mock_logger_debug.called)
             mock_add_devices_fn.assert_called_with(ports)
             self.assertIn(FAKE_PORT_1, self.agent.cluster_other_ports)
@@ -1395,7 +1395,7 @@ class TestOVSvAppAgent(base.TestCase):
             self.agent.device_create(FAKE_CONTEXT,
                                      device=DEVICE,
                                      ports=ports,
-                                     sg_rules=mock.MagicMock())
+                                     sg_rules=FAKE_SG_RULES)
             self.assertTrue(mock_logger_debug.called)
             self.assertNotIn(FAKE_PORT_1, self.agent.cluster_other_ports)
             self.assertIn(FAKE_PORT_1, self.agent.cluster_host_ports)
@@ -1428,7 +1428,7 @@ class TestOVSvAppAgent(base.TestCase):
             self.agent.device_create(FAKE_CONTEXT,
                                      device=DEVICE,
                                      ports=ports,
-                                     sg_rules=mock.MagicMock())
+                                     sg_rules=FAKE_SG_RULES)
             self.assertTrue(mock_logger_debug.called)
             self.assertNotIn(FAKE_PORT_1, self.agent.cluster_other_ports)
             self.assertIn(FAKE_PORT_1, self.agent.cluster_host_ports)
@@ -1462,7 +1462,7 @@ class TestOVSvAppAgent(base.TestCase):
             self.agent.device_create(FAKE_CONTEXT,
                                      device=DEVICE,
                                      ports=ports,
-                                     sg_rules=mock.MagicMock())
+                                     sg_rules=FAKE_SG_RULES)
             self.assertTrue(mock_logger_debug.called)
             self.assertNotIn(FAKE_PORT_1, self.agent.cluster_other_ports)
             self.assertIn(FAKE_PORT_1, self.agent.cluster_host_ports)
@@ -1501,7 +1501,7 @@ class TestOVSvAppAgent(base.TestCase):
             self.agent.device_create(FAKE_CONTEXT,
                                      device=DEVICE,
                                      ports=ports,
-                                     sg_rules=mock.MagicMock())
+                                     sg_rules=FAKE_SG_RULES)
             self.assertTrue(mock_populate_tun_flows.called)
             self.assertTrue(mock_logger_debug.called)
             self.assertNotIn(FAKE_PORT_1, self.agent.cluster_other_ports)
@@ -1541,7 +1541,7 @@ class TestOVSvAppAgent(base.TestCase):
             self.agent.device_create(FAKE_CONTEXT,
                                      device=DEVICE,
                                      ports=ports,
-                                     sg_rules=mock.MagicMock())
+                                     sg_rules=FAKE_SG_RULES)
             self.assertTrue(mock_populate_tun_flows.called)
             self.assertTrue(mock_logger_debug.called)
             self.assertNotIn(FAKE_PORT_1, self.agent.cluster_other_ports)
