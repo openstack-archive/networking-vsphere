48014b1cf72908c00bc03baa992baafb73d0b05d
M Arun Kumar <madheven-arun.kumar@hpe.com>
2016-08-06 09:43:06 +0530

    vif binding failed for instances deployed in esx compute
    Issue with ovsvapp agent due to upstream code changes.
    1> canary flow table returns additional text which needs to be handled in ovsvapp agent.
    2> Change in ovs agent related to vlan maps
    
    Change-Id: Icba1bfcd5ebcae659b5110cedcba64a03d80868b
    Closes-Bug: #1613945

--- networking_vsphere/tests/unit/agent/test_ovsvapp_agent.py
+++ networking_vsphere/tests/unit/agent/test_ovsvapp_agent.py
@@ -32,12 +32,14 @@ from neutron.agent.common import ovs_lib
 from neutron.common import utils as n_utils
 from neutron.plugins.common import constants as p_const
 from neutron.plugins.common import utils as p_utils
+from neutron.plugins.ml2.drivers.openvswitch.agent.common import constants as ovs_const  # noqa
 from neutron.plugins.ml2.drivers.openvswitch.agent import ovs_neutron_agent as ovs_agent  # noqa
 from neutron.plugins.ml2.drivers.openvswitch.agent import vlanmanager
 
 NETWORK_ID = 'fake_net_id'
 VNIC_ADDED = 'VNIC_ADDED'
 FAKE_DEVICE_ID = 'fake_device_id'
+FAKE_LVID = '1'
 FAKE_VM = 'fake_vm'
 FAKE_HOST_1 = 'fake_host_1'
 FAKE_HOST_2 = 'fake_host_2'
@@ -51,6 +53,7 @@ FAKE_PORT_3 = 'fake_port_3'
 FAKE_PORT_4 = 'fake_port_4'
 MAC_ADDRESS = '01:02:03:04:05:06'
 FAKE_CONTEXT = 'fake_context'
+FAKE_SEG_ID = '1001'
 FAKE_SG = {'fake_sg': 'fake_sg_rule'}
 
 FAKE_SG_RULE = {'security_group_source_groups': ['fake_rule_1',
@@ -136,6 +139,67 @@ class SamplePortUIDMac(object):
     def __init__(self, port_uuid, mac_address):
         self.port_uuid = port_uuid
         self.mac_address = mac_address
+
+
+class TestOVSvAppAgentRestart(base.TestCase):
+
+    @mock.patch('neutron.common.config.init')
+    @mock.patch('neutron.common.config.setup_logging')
+    @mock.patch('neutron.agent.ovsdb.api.'
+                'API.get')
+    @mock.patch('networking_vsphere.agent.ovsvapp_agent.RpcPluginApi')
+    @mock.patch('neutron.agent.securitygroups_rpc.SecurityGroupServerRpcApi')
+    @mock.patch('neutron.agent.rpc.PluginReportStateAPI')
+    @mock.patch('networking_vsphere.agent.ovsvapp_agent.OVSvAppPluginApi')
+    @mock.patch('neutron.context.get_admin_context_without_session')
+    @mock.patch('neutron.agent.rpc.create_consumers')
+    @mock.patch('neutron.plugins.ml2.drivers.openvswitch.agent.'
+                'ovs_neutron_agent.OVSNeutronAgent.setup_integration_br')
+    @mock.patch('networking_vsphere.agent.ovsvapp_agent.'
+                'OVSvAppAgent.setup_ovs_bridges')
+    @mock.patch('networking_vsphere.agent.ovsvapp_agent.'
+                'OVSvAppAgent.setup_security_br')
+    @mock.patch('networking_vsphere.agent.ovsvapp_agent.'
+                'OVSvAppAgent._init_ovs_flows')
+    @mock.patch('networking_vsphere.drivers.ovs_firewall.OVSFirewallDriver.'
+                'check_ovs_firewall_restart')
+    @mock.patch('networking_vsphere.drivers.ovs_firewall.'
+                'OVSFirewallDriver.setup_base_flows')
+    @mock.patch('neutron.agent.common.ovs_lib.OVSBridge.create')
+    @mock.patch('neutron.agent.common.ovs_lib.OVSBridge.set_secure_mode')
+    @mock.patch('neutron.agent.common.ovs_lib.OVSBridge.get_port_ofport')
+    def setUp(self, mock_get_port_ofport,
+              mock_set_secure_mode, mock_create_ovs_bridge,
+              mock_setup_base_flows, mock_check_ovs_firewall_restart,
+              mock_init_ovs_flows, mock_setup_security_br,
+              mock_setup_ovs_bridges,
+              mock_setup_integration_br, mock_create_consumers,
+              mock_get_admin_context_without_session, mock_ovsvapp_pluginapi,
+              mock_plugin_report_stateapi, mock_securitygroup_server_rpcapi,
+              mock_rpc_pluginapi, mock_ovsdb_api, mock_setup_logging,
+              mock_init):
+        super(TestOVSvAppAgentRestart, self).setUp()
+        cfg.CONF.set_override('security_bridge_mapping',
+                              "fake_sec_br:fake_if", 'SECURITYGROUP')
+        mock_get_port_ofport.return_value = 5
+        self.agent = ovsvapp_agent.OVSvAppAgent()
+        self.agent.run_refresh_firewall_loop = False
+        self.LOG = ovsvapp_agent.LOG
+        self.agent.monitor_log = logging.getLogger('monitor')
+
+    def test_check_ovsvapp_agent_restart(self):
+        self.agent.int_br = mock.MagicMock()
+        with mock.patch.object(self.agent.int_br, 'bridge_exists',
+                               return_value=True) as mock_br_exists, \
+                mock.patch.object(self.agent.int_br, 'dump_flows_for_table',
+                                  return_value='') as mock_dump_flows:
+            self.assertFalse(self.agent.check_ovsvapp_agent_restart())
+            self.assertTrue(mock_br_exists.called)
+            self.assertTrue(mock_dump_flows.called)
+            mock_dump_flows.return_value = 'cookie = 0x0'
+            self.assertTrue(self.agent.check_ovsvapp_agent_restart())
+            self.assertTrue(mock_br_exists.called)
+            self.assertTrue(mock_dump_flows.called)
 
 
 class TestOVSvAppAgent(base.TestCase):
